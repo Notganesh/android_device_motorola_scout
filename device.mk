e0274841e13059eac0bd0b87b83cbac513b4b30a
SahilSonar <sss.sonar2003@gmail.com>
2024-09-04 18:33:14 +0530

    scout: Add kernel flags
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -304,7 +304,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.scout-multihal
 
 # Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := 34
+
+# Kernel
+$(call inherit-product, device/motorola/scout-kernel/kernel.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
