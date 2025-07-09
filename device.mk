c0a5d4b27ff9f18b65cd73c5c3fd835e8f7d6fb0
sabrina010209 <sebidk700@gmail.com>
2025-07-08 19:43:37 -0500

    scout: Rename fingerprint wrapper
    * To avoid conflicts with egis one
    
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -137,7 +137,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    com.motorola.biometrics.fingerprint@1.0.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.gdx
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
@@ -216,7 +216,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.scout
+    android.hardware.light-service.scout
 
 # Media
 PRODUCT_COPY_FILES += \
