a25c3f967268004010c0bb0e062d9cfd4a74ea48
claxten10 <136853788+claxten10@users.noreply.github.com>
2025-10-08 14:56:40 +0530

    scout: Configure bluetooth stack
    Signed-off-by: Notganesh <chimeports@gmail.com>
    Change-Id: Ibfca1370a9ec7ee8664a3d6d0ea0efd9a306d368

--- device.mk
+++ device.mk
@@ -57,6 +57,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth-V1-ndk.vendor \
+    android.hardware.bluetooth.audio-V3-ndk.vendor \
+    android.hardware.bluetooth.audio-impl \
+    libbluetooth_audio_session \
+    android.hardware.bluetooth@1.1.vendor
+
 # Bootcontrol
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
