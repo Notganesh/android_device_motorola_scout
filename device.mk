c18914f4da9574889d99a7e6fd397985848bbd7d
Notganesh <chimeports@gmail.com>
2025-10-09 11:58:56 +0530

    scout: configure thermal stack
    Signed-off-by: Notganesh <chimeports@gmail.com>
    Change-Id: Ibfca1370a9ec7ee8664a3d6d0ea0efd9a306d368

--- device.mk
+++ device.mk
@@ -205,6 +205,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.scout \
